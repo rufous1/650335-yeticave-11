3c3990fa7768b43f75920d112d88f3fb279a5069
Rufous <dshishov89@gmail.com>
2019-10-26 13:56:47 +0300

    правка замечания

--- index.php
+++ index.php
@@ -45,7 +45,6 @@ $goods = [
 
 function formatPrice(int $number): string {
     $number = ceil($number);
-
     $formatNumber = ($number < 1000) ? $number : number_format($number, 0, '.', ' ');
 
     return $formatNumber . ' ₽';
