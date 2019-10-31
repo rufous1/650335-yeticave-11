69ddda1466c53f02fea4f52dd6741a7e4fdadc46
Rufous <dshishov89@gmail.com>
2019-10-31 23:34:02 +0300

    добавляет дату в массив

--- data.php
+++ data.php
@@ -8,37 +8,43 @@ $lots = [
     'title' => '2014 Rossignol District Snowboard',
     'category' => $categories[0],
     'price' => 10999,
-    'image' => 'img/lot-1.jpg'
+    'image' => 'img/lot-1.jpg',
+    'lotTaimer' => '2019-11-04'
   ],
   [
     'title' => 'DC Ply Mens 2016/2017 Snowboard',
     'category' => $categories[0],
     'price' => 159999,
-    'image' => 'img/lot-2.jpg'
+    'image' => 'img/lot-2.jpg',
+    'date' => '2019-11-02'
   ],
   [
     'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
     'category' => $categories[1],
     'price' => 8000,
-    'image' => 'img/lot-3.jpg'
+    'image' => 'img/lot-3.jpg',
+    'date' => '2019-11-10'
   ],
   [
     'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
     'category' => $categories[2],
     'price' => 10999,
-    'image' => 'img/lot-4.jpg'
+    'image' => 'img/lot-4.jpg',
+    'date' => '2019-11-22'
   ],
   [
     'title' => 'Куртка для сноуборда DC Mutiny Charocal',
     'category' => $categories[3],
     'price' => 7500,
-    'image' => 'img/lot-5.jpg'
+    'image' => 'img/lot-5.jpg',
+    'date' => '2019-12-03'
   ],
   [
     'title' => 'Маска Oakley Canopy',
     'category' => $categories[5],
     'price' => 5400,
-    'image' => 'img/lot-6.jpg'
+    'image' => 'img/lot-6.jpg',
+    'date' => '2019-11-01'
   ]
 ];
 ?>
