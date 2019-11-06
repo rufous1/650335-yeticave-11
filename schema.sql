b9736855f067d4bc161402441b8c9e4874509261
Rufous <dshishov89@gmail.com>
2019-11-06 15:55:22 +0300

    Добавляет SQL-запрос создания БД

--- schema.sql
+++ schema.sql
@@ -6,30 +6,28 @@ USE yeticave;
 
 CREATE TABLE categories (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  title CHAR(32) NOT NULL,
-  character_code CHAR(20) NOT NULL
+  title VARCHAR(32) NOT NULL,
+  character_code VARCHAR(20)
 );
 
 CREATE TABLE users (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   date_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  name CHAR(64) NOT NULL,
+  name VARCHAR(64) NOT NULL,
   email VARCHAR(128) NOT NULL UNIQUE,
   password CHAR(64) NOT NULL,
-  user_contacts_id INT NOT NULL
 );
 
 CREATE TABLE lots (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   date_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   title VARCHAR(255) NOT NULL,
-  description TEXT NOT NULL,
-  image_lots_id INT NOT NULL,
+  description TEXT,
   first_price INT UNSIGNED NOT NULL,
   date_end TIMESTAMP NOT NULL,
   step TINYINT UNSIGNED NOT NULL,
   user_id INT NOT NULL,
-  winner_id INT NOT NULL,
+  winner_id INT,
   category_id INT NOT NULL
 );
 
@@ -43,17 +41,18 @@ CREATE TABLE bets (
 
 CREATE TABLE user_contacts (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  users_id INT,
   city VARCHAR(32) NOT NULL,
   address VARCHAR(255) NOT NULL,
-  phone TINYINT NOT NULL
+  phone TINYINT UNIQUE NOT NULL
 );
 
-CREATE TABLE image_lots (
+CREATE TABLE lot_images (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  image VARCHAR(255) NOT NULL
+  lots_id INT,
+  image VARCHAR(255) NOT NULL,
 );
 
 CREATE UNIQUE INDEX user_email ON users(email);
 CREATE INDEX lot_search ON lots(title);
 CREATE INDEX bet ON bets(bet);
-
